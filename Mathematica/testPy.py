fa40c400f5bcecd1118f9b98330c24c460bb360a
dansmaranda <d.smaranda.1@research.gla.ac.uk>
2018-09-21 16:30:10 +0100

    Wrote the alpha version of the SO11 masses script, seems to work all right ish.

--- Mathematica/testPy.py
+++ Mathematica/testPy.py
@@ -3,21 +3,36 @@ import subprocess
 import time
 from colorama import Fore, Back, Style
 import os
+import json
 
+
+from sympy import *
+from sympy.parsing import mathematica
+# sa, x = symbols("a x")
+
+# mathParse = mathematica.mathematica()
+# print (type(mathParse))
+# print (mathParse)
+def genNumbers():
+	'''
+	'''
+	pass
 
 def workerFct(threadNb, debug):
 	# subprocess.ca
 	colorDict= {'0':Fore.RED, '1':Fore.GREEN, '2':Fore.BLUE, '3':Fore.YELLOW}
 	if debug:
 		print ("Debug On!!")
-	
+
 	FNULL = open(os.devnull, 'w')
 
 	while True:
 		print (colorDict[str(threadNb % 4) ] + "Mathematica Out from Thread number: ", threadNb)
 		try:
-			subprocess.call('wolframscript -script SO11_Masses.m', shell = True, stdout=FNULL, stderr=subprocess.STDOUT)
+			subprocess.call('./SO11_Masses.m ThreadNb-' + str(threadNb), shell = True, stdout = FNULL,  stderr=subprocess.STDOUT)
 			time.sleep(0.1 + threadNb/10)
+
+			#  Get fron out dict .
 		except:
 			break
 			print ('No Bueno!!!!!!!!!!!!!!!!!!!!!!!!')
@@ -25,7 +40,14 @@ def workerFct(threadNb, debug):
 		print (Style.RESET_ALL)
 
 if __name__=='__main__':
+
 	processes = [Process(target=workerFct, args=(int(threadNb), False) ) for threadNb in range(16) ]
+
+
+	dataDictOut = {'k':89000, 'zL':35}
+	with open("dataIn.json", 'w') as outFile:
+	    json.dump(dataDictOut, outFile)
+
 
 	for proc in processes:
 		proc.start()
